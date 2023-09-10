8d28eed93c3e98f4bf2155e612bc5fc06ba120ac
devjorgesousa <144136972+devjorgesousa@users.noreply.github.com>
2023-09-10 16:26:16 -0300

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// zoom da imagem
+function aumenta(obj){
+    obj.height=obj.height*2;
+    obj.width=obj.width*2;
+}
+
+function diminui(obj){
+    obj.height=obj.height/2;
+    obj.width=obj.width/2;
+}
